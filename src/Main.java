63edf1d41767d57de53582ef35381a39fbf7647d
prathamkumar <prathamkumar1985@gmail.com>
2023-07-05 02:33:13 +0530

    Make changes

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,14 @@
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
-
+    public static void main(String args[]){
+        int arr[] = {10,20,30,40,50};
+        int key = 30;
+        int result = Arrays.binarySearch(arr,key);
+        if (result < 0)
+            System.out.println("Element is not found!");
+        else
+            System.out.println("Element is found at index: "+result);
     }
 }
