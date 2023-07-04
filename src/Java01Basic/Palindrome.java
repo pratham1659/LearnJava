4c8111313dbd4f0ce2b8440a15cef0b8a167ca13
prathamkumar <prathamkumar1985@gmail.com>
2023-07-04 10:38:20 +0530

    Make changes

--- src/Java01Basic/Palindrome.java
+++ src/Java01Basic/Palindrome.java
@@ -1,7 +1,11 @@
 package Java01Basic;
 
+import java.util.Scanner;
+
 public class Palindrome {
     public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
 
 
     }
