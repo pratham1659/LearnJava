6ad53716b41143784d208a43b464c32a9e0b498c
prathamkumar <prathamkumar1985@gmail.com>
2024-02-01 02:05:15 +0530

    Oops Concepts

---
+++ src/Java08Oops/Java03Encapsulation/publicClass/JavaPublicClass.java
@@ -0,0 +1,7 @@
+package Java08Oops.Java03Encapsulation.publicClass;
+
+public class JavaPublicClass {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/Java08Oops/Java03Encapsulation/questions/Geometry/Circles.java
@@ -0,0 +1,13 @@
+package Java08Oops.Java03Encapsulation.questions.Geometry;
+
+public class Circles {
+    public double radius;
+
+    Circles(){
+
+    }
+
+    public Circles(double radius) {
+        this.radius = radius;
+    }
+}
