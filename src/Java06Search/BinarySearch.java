b7cfcb39589de18538e21e2d21557b9ef6aac429
prathamkumar <prathamkumar1985@gmail.com>
2024-04-17 10:44:19 +0530

    Sorting Update questions

--- src/Java06Search/BinarySearch.java
+++ src/Java06Search/BinarySearch.java
@@ -53,87 +53,86 @@ public class BinarySearch {
         return -1;
     }
 
-    static int recursiveBinarySearch(int arr[], int start, int end, int target) {
-        if (end >= start) {
+    public static int recursiveBinarySearch(int[] arr, int left, int right, int target) {
+        if (left <= right) {
 
-            int mid = start + (end - start) / 2; //better way to prevent overflow in binary search.
+            int mid = left + (right - left) / 2; //better way to prevent overflow in binary search.
 
             if (arr[mid] == target)
                 return mid;
 
-            if (arr[mid] > target)
-                return recursiveBinarySearch(arr, start, mid - 1, target);
+            if (arr[mid] < target) {
+                return recursiveBinarySearch(arr, mid + 1, right, target);
+            }
 
-            return recursiveBinarySearch(arr, mid + 1, end, target);
+            return recursiveBinarySearch(arr, left, mid - 1, target);
         }
         return -1;
     }
 
 
-    //    Find the first occurrence of a given element x, given that the given array is sorted. If no occurrence
-//    of x is found then return -1.
+    // Find the first occurrence of a given element x, given that the given array is sorted.
+    // If no occurrence of x is found, then return -1.
     public static int findFirstOccurrence(int[] arr, int target) {
         int len = arr.length;
         int start = 0;
         int end = len - 1;
-        int firstOccur = -1;
+        int firstIndex = -1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            if (target == arr[mid]) {
-                firstOccur = mid;
+            if (arr[mid] == target) {
+                firstIndex = mid;
                 end = mid - 1;
-                return mid;
-            } else if (target < arr[mid]) {
-                end = mid - 1;
-            } else {
+            } else if (arr[mid] < target) {
                 start = mid + 1;
+            } else {
+                end = mid - 1;
             }
         }
-        return firstOccur;
+        return firstIndex;
     }
 
     public static int findLastOccurrence(int[] arr, int target) {
         int len = arr.length;
         int start = 0;
         int end = len - 1;
-        int lastOccur = -1;
+        int lastIndex = -1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
             if (target == arr[mid]) {
-                lastOccur = mid;
+                lastIndex = mid;
                 start = mid + 1;
-            } else if (target < arr[mid]) {
-                end = mid - 1;
+            } else if (arr[mid] < target) {
+                start = mid + 1;
             } else {
-                start = mid + 1;
+                end = mid - 1;
             }
         }
-        return lastOccur;
+        return lastIndex;
     }
 
-//    Find the square root of the given non negative value x. Round it off to the nearest floor integer value.
-
+    // Find the square root of given non-negative value x. Round it off to the nearest floor integer.
     public static int squareRootBinarySearch(int target) {
         int start = 0, end = target;
         int ans = -1;
 
         while (start <= end) {
             int mid = start + (end - start) / 2;
-            long value = (long) mid * mid;
+            long value = (long) mid * mid; // Calculate square using long to prevent overflow
+
             if (value == target) {
-                return mid;
+                return mid; // Found exact square root
             } else if (value < target) {
-                ans = mid;
-                start = mid + 1;
+                ans = mid; // Update answer to potential square root
+                start = mid + 1; // Search in the right half
             } else {
-                end = mid - 1;
-                ans = mid;
+                end = mid - 1; // Search in the left half
             }
         }
 
-        return ans;
+        return ans; // Return the nearest floor square root
     }
 
     public static void main(String[] args) {
@@ -151,28 +150,15 @@ public class BinarySearch {
         arrayList.add(14);
 
         System.out.println("Binary Search ArrayList: " + binarySearchArrayList(arrayList, 10));
+        System.out.println("Recursion Binary: " + recursiveBinarySearch(arr, 0, arr.length - 1, 12));
 
 
-//        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8, 9};
-//        int[] arr1 = {2, 4, 5, 9, 12, 16, 19};
-//        int target = 4;
-//
-////        int result = binarySearch(arr, target);
-//        int result = recursiveBinarySearch(arr, 0, arr.length - 1, target);
-//        if (result == -1)
-//            System.out.println("Element not present");
-//        else
-//            System.out.println("Element found at index " + result);
+        int[] findArr = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
+        int target = 5;
+        System.out.println("First Occurrence index: " + findFirstOccurrence(findArr, target));
+        System.out.println("Last Occurrence index: " + findLastOccurrence(findArr, target));
 
+        System.out.println("Square Root: " + squareRootBinarySearch(12));
 
-//        int[] arr = {2, 5, 5, 5, 6, 6, 8, 9, 9, 9};
-//        int target = 5;
-////        int result = findFirstOccurrence(arr, target);
-//        int result = findLastOccurrence(arr, target);
-//        if (result == -1)
-//            System.out.println("Element not present");
-//        else
-//            System.out.println("Element found at index " + result);
     }
-
 }
--- src/Java07Sorting/BubbleSort.java
+++ src/Java07Sorting/BubbleSort.java
@@ -2,63 +2,105 @@ package Java07Sorting;
 
 public class BubbleSort {
 
-    public static void increaseSort(int[] arr){
-        int len = arr.length;
 
-        for(int i=0; i<len-1; i++){
-            for(int j=0; j<len-i-1; j++){
-                if(arr[j] > arr[j+1]){
+    //Ques 1: Program to Sort Array increasing Order
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(1)
+    public static void increaseSort(int[] arr) {
+        int len = arr.length;
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
-    public static void decreaseSort(int[] arr){
+    //Ques 2: Program to Sort Array decreasing Order
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(1)
+    public static void decreaseSort(int[] arr) {
         int len = arr.length;
 
-        for(int i=0; i<len-1; i++){
-            for(int j=0; j<len-i-1; j++){
-                if(arr[j] < arr[j+1]){
+        for (int i = 0; i < len - 1; i++) {
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] < arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                 }
             }
         }
     }
 
-    public static void optimiseBubble(int[] arr){
+    //Ques 2: Program to Sort Array increasing Order another Approach
+    // Time Complexity: O(N^2)
+    // Space Complexity: O(1)
+    public static void sorting(int[] arr){
         int len = arr.length;
 
-        for (int i = 0; i < len-1; i++) {
+        for(int i = 0; i < arr.length; i++){
+            for(int j = i + 1; j < arr.length; j++){
+                if(arr[i] > arr[j]){
+                    int temp = arr[i];
+                    arr[i] = arr[j];
+                    arr[j] = temp;
+                }
+            }
+        }
+
+    }
+
+    public static void optimiseBubble(int[] arr) {
+        int len = arr.length;
+
+        for (int i = 0; i < len - 1; i++) {
             boolean flag = false;
-            for (int j = 0; j < len-i-1; j++) {
-                if(arr[j] > arr[j+1]){
+            for (int j = 0; j < len - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
                     int temp = arr[j];
-                    arr[j] = arr[j+1];
-                    arr[j+1] = temp;
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = temp;
                     flag = true;
                 }
             }
-            if(!flag){  //Have any swaps Happened
+            if (!flag) {  //Have any swaps Happened
                 return;
             }
         }
     }
+
+    //printArray Approach
+    public static void printArray(int[] arr) {
+        for (int i : arr) {
+            System.out.print(i + " ");
+        }
+        System.out.println();
+    }
+
     public static void main(String[] args) {
 
-//        int[] arr = {4, 1, 2, 3, 5, 6};
-        int[] arr = { 4, 3, 5, 4, 2, 1};
 
-//        increaseSort(arr);
-//        decreaseSort(arr);
+
+        int[] arr = {64, 34, 25, 12, 22, 11, 90};
+        System.out.print("Increasing Order: ");
+        increaseSort(arr);
+        printArray(arr);
+
+        System.out.print("Decreasing Order: ");
+        decreaseSort(arr);
+        printArray(arr);
+
+        System.out.println("Another Approach: ");
+        sorting(arr);
+        printArray(arr);
+
 
         optimiseBubble(arr);
-
-        for(int i : arr){
+        for (int i : arr) {
             System.out.print(i + " ");
         }
 
