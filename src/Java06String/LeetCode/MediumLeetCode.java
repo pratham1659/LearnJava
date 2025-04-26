1b9069a21f2c1a9d58ed73caa867cde9e339bcba
prathamkumar <prathamkumar1985@gmail.com>
2025-04-26 09:27:41 +0530

    #Leetcode added

--- src/Java06String/LeetCode/MediumLeetCode.java
+++ src/Java06String/LeetCode/MediumLeetCode.java
@@ -1,5 +1,46 @@
 package Java06String.LeetCode;
 
 public class MediumLeetCode {
-    
+
+    public class LongestPalindromeSubstring {
+
+        // Function to check if a string is a palindrome
+        public static boolean isPalindrome(String str) {
+            int left = 0, right = str.length() - 1;
+
+            while (left < right) {
+                if (str.charAt(left) != str.charAt(right)) {
+                    return false;
+                }
+                left++;
+                right--;
+            }
+            return true;
+        }
+
+        // Function to find the longest palindromic substring using substring
+        public static String longestPalindrome(String s) {
+            int n = s.length();
+            String longest = "";
+
+            for (int i = 0; i < n; i++) {
+                for (int j = i + 1; j <= n; j++) {
+                    String sub = s.substring(i, j); // substring from index i to j-1
+                    if (isPalindrome(sub) && sub.length() > longest.length()) {
+                        longest = sub;
+                    }
+                }
+            }
+
+            return longest;
+        }
+
+        public static void main(String[] args) {
+
+            String input = "aaaabaaa";
+            String result = longestPalindrome(input);
+            System.out.println(result);
+
+        }
+    }
 }
--- src/Test.java
+++ src/Test.java
@@ -3,53 +3,55 @@ import java.util.*;
 
 public class Test {
 
-    public static void practice(String str) {
+    public static void minMedals(int[] ranks) {
+        int n = ranks.length;
 
-       
-    }
+        // Array to store the number of medals for each soldier
+        int[] medals = new int[n];
 
-    public static boolean isPalindrome(String str) {
-        int left = 0;
-        int right = str.length() - 1;
-
-        while (left < right) {
-            if (str.charAt(left) != str.charAt(right)) {
-                return false;
-            }
-            left++;
-            right--;
+        // Step 1: Assign 1 medal to each soldier initially
+        for (int i = 0; i < n; i++) {
+            medals[i] = 1;
         }
-        return true;
-    }
 
-    public static ArrayList<String> findPalindromes(String bigString) {
-        ArrayList<String> palindromes = new ArrayList<>();
-
-        // Iterate through each substring and check for palindrome
-        for (int i = 0; i < bigString.length(); i++) {
-            for (int j = i + 1; j <= bigString.length(); j++) {
-                String substring = bigString.substring(i, j);
-                if (isPalindrome(substring)) {
-                    palindromes.add(substring);
-                }
+        // Step 2: Left to Right: Ensure that soldiers with higher ranks get more medals
+        // than their left neighbor
+        for (int i = 1; i < n; i++) {
+            if (ranks[i] > ranks[i - 1]) {
+                medals[i] = medals[i - 1] + 1;
             }
         }
 
-        return palindromes;
+        // Step 3: Right to Left: Ensure that soldiers with higher ranks get more medals
+        // than their right neighbor
+        for (int i = n - 2; i >= 0; i--) {
+            if (ranks[i] > ranks[i + 1]) {
+                medals[i] = Math.max(medals[i], medals[i + 1] + 1);
+            }
+        }
+
+        // Step 4: Sum up the medals to get the minimum total number of medals
+        int totalMedals = 0;
+        for (int i = 0; i < n; i++) {
+            totalMedals += medals[i];
+        }
+
+        System.out.println(totalMedals);
     }
 
     public static void main(String[] args) throws IOException {
 
         Scanner sc = new Scanner(System.in);
 
-        String str = sc.nextLine();
-        // int n = sc.nextInt();
+        // String str = sc.nextLine();
+        // String str2 = sc.nextLine();
+        int n = sc.nextInt();
 
-        // int[] arr = new int[n];
+        int[] arr = new int[n];
 
-        // for (int i = 0; i < n; i++) {
-        // arr[i] = sc.nextInt();
-        // }
+        for (int i = 0; i < n; i++) {
+            arr[i] = sc.nextInt();
+        }
 
         // ArrayList<Integer> arr = new ArrayList<>();
 
@@ -59,12 +61,8 @@ public class Test {
         // arr.add(8);
         // arr.add(9);
         // String[] str = { "Java", "JavaScript", "Python", "Ruby", "Cpp", "Java" };
-        practice(str);
+        minMedals(arr);
 
-         String bigString = "abcbadefedabcba"; // Example big string
-        ArrayList<String> palindromes = findPalindromes(bigString);
-
-        System.out.println(palindromes.toString());
         // System.out.println(practice("{[()]}"));
         // System.out.println(practice(str));
 
